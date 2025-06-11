4f0f25aa0edc18a7864b34c50a178f378053d143
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-06-11 22:16:29 +0100

    Category, Todo and User Repositories (interfaces) to interact with the DB throw some methods: findCategoryByUserId, findCategoryByTodoId and getAllTodoByCategoriesForToday

---
+++ Backend/todo/src/main/java/com/alexandre/todo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alexandre.todo.repositories;
+
+import com.alexandre.todo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUsersByEmailAAndPassword(String email, String password);
+}
